a180c053ca656b5c0cdf196f872df6b4182f73ce
Silverlan <silverlann@gmail.com>
2020-09-14 10:07:34 +0200

    Add warning messages in case of errors

--- src/main.cpp
+++ src/main.cpp
@@ -25,10 +25,16 @@ int main(int argc,char *argv[])
 	std::string err;
 	auto lib = util::Library::Load(libPath.GetString(),{path.GetString()},&err);
 	if(lib == nullptr)
+	{
+		std::cout<<"Unable to load library '"<<libPath.GetString()<<"': "<<err<<std::endl;
 		return EXIT_FAILURE;
+	}
 	auto *f = lib->FindSymbolAddress<int(*)(int,char*[])>("render_raytracing");
 	if(f == nullptr)
+	{
+		std::cout<<"Unable to locate symbol address for 'render_raytracing' in library '"<<libPath.GetString()<<"'!"<<std::endl;
 		return EXIT_FAILURE;
+	}
 	auto result = f(argc,argv);
 
 	lib = nullptr;
