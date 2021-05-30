bfe04eb6cc1c064f4dfd048da0b447dece48fb02
Silverlan <silverlann@gmail.com>
2021-05-30 12:41:31 +0200

    no message

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,23 @@
 #include <sharedutils/util_library.hpp>
 #include <sharedutils/util_path.hpp>
+#include <sharedutils/util.h>
 #include <fsys/filesystem.h>
 
 #pragma optimize("",off)
 int main(int argc,char *argv[])
 {
+	auto programPath = util::Path::CreatePath(util::get_program_path());
+	programPath.PopBack(); // Go up from "bin" directory
+	util::set_program_path(programPath.GetString());
+	util::set_current_working_directory(programPath.GetString());
+
 	auto path = util::Path::CreatePath(FileManager::GetRootPath());
-	path.PopBack();
 	path += "modules/cycles";
 
 	auto libPath = util::Path::CreatePath(FileManager::GetProgramPath());
-	libPath += "render_raytracing_lib.dll";
+	libPath += "bin/render_raytracing_lib.dll";
 
 	auto fileRootPath = util::Path::CreatePath(FileManager::GetRootPath());
-	fileRootPath.PopBack();
 	FileManager::SetAbsoluteRootPath(fileRootPath.GetString());
 
 	FileManager::AddCustomMountDirectory("materials");
@@ -37,6 +41,9 @@ int main(int argc,char *argv[])
 	}
 	auto result = f(argc,argv);
 
+	// TODO: We'll force an exit, since doing a clean exit causes it to permanently freeze
+	// Fix this issue!
+	exit(EXIT_SUCCESS);
 	lib = nullptr;
 	return result;
 }
